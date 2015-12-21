1c967fd32f3434e20b2a8bfe480fa1a3706e0416
iraritchiemeek <iraritchiemeek@gmail.com>
2015-12-21 13:01:03 +1300

    add service sections

--- js/services_page.js
+++ js/services_page.js
@@ -7,6 +7,15 @@ ServicesPage.prototype.setupDivs = function() {
 	$('#content').append('<div id="services_info"></div>')
 };
 
-ServicesPage.prototype.serviceItem = function(serviceName, imageUrl) {
+ServicesPage.prototype.serviceItem = function(serviceName, serviceInfo, imageUrl) {
+	this.serviceButton(serviceName, imageUrl)
+	this.serviceSection(serviceName, serviceInfo, imageUrl)
+};
+
+ServicesPage.prototype.serviceButton = function(serviceName, imageUrl) {
 	$('#services_buttons').append('<div style="background-image: url(' + imageUrl + ')" class="service_button" id="' + serviceName + '_button"><h1>' + serviceName + '</h1></div>')
+};
+
+ServicesPage.prototype.serviceSection = function(serviceName, serviceInfo, imageUrl) {
+	$('#services_info').append('<div style="background-image: url(' + imageUrl + ')" class="service_section" id="' + serviceName + '_section"><h1>' + serviceName + '</h1><p>' + serviceInfo + '</p></div>')
 };
