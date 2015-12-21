6b119eed2e29825d9102472e2a8d8b5253e70ceb
iraritchiemeek <iraritchiemeek@gmail.com>
2015-12-21 15:27:28 +1300

    add scrollto

--- js/services_page.js
+++ js/services_page.js
@@ -13,9 +13,14 @@ ServicesPage.prototype.serviceItem = function(serviceName, serviceInfo, imageUrl
 };
 
 ServicesPage.prototype.serviceButton = function(serviceName, imageUrl) {
-	$('#services_buttons').append('<div style="background-image: url(' + imageUrl + ')" class="service_button" id="' + serviceName + '_button"><h1>' + serviceName + '</h1></div>')
+	$('#services_buttons').append('<div style="background-image: url(' + imageUrl + ')" class="service_button" id="' + serviceName.split(' ').join('_') + '"><h1>' + serviceName + '</h1></div>')
 };
 
 ServicesPage.prototype.serviceSection = function(serviceName, serviceInfo, imageUrl) {
-	$('#services_info').append('<div style="background-image: url(' + imageUrl + ')" class="service_section" id="' + serviceName + '_section"><h1>' + serviceName + '</h1><p>' + serviceInfo + '</p></div>')
+	$('#services_info').append('<div style="background-image: url(' + imageUrl + ')" class="service_section" id="' + serviceName.split(' ').join('_') + '_section"><h1>' + serviceName + '</h1><p>' + serviceInfo + '</p></div>')
+};
+
+ServicesPage.prototype.scrollTo = function(id) {
+	// console.log($('#'+ id + '_section')[0])
+	$.scrollTo($('#'+ id + '_section')[0], {duration: '1000', offset: -100})
 };
