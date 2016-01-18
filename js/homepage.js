85e3e111cfff940c48efc7a29bc118327077d94f
iraritchiemeek <iraritchiemeek@gmail.com>
2016-01-18 14:42:08 +1300

    Made default page the homepage and add landing video

--- js/homepage.js
+++ js/homepage.js
@@ -1,7 +1,11 @@
-function Homepage () {
+function HomePage () {
 	
 }
 
-Homepage.prototype.setupSection = function(id) {
-	$('#content').append('<div id="' + id + '" class="homepage_section"></div>')
+HomePage.prototype.homepageVideo = function(url) {
+	$('#homepage_video').append('<video autoplay="" loop="" name="media"><source src="' + url + '" type="video/mp4"></video></div>')
+};
+
+HomePage.prototype.setupSection = function(id, additional_class) {
+	$('#content').append('<div id="' + id + '" class="homepage_section ' + additional_class + '"></div>')
 };
