b403f4bd9e59772be79191edbdac81e186c041c6
iraritchiemeek <iraritchiemeek@gmail.com>
2016-01-22 09:55:53 +1300

    remove old services CSS, begin adding new services sections

--- js/services_page.js
+++ js/services_page.js
@@ -1,3 +1,7 @@
 function ServicesPage () {
 	
 }
+
+ServicesPage.prototype.setupSection = function(id, additional_class) {
+	$('#content').append('<div id="' + id + '" class="services_section ' + additional_class + '"></div>')
+};
